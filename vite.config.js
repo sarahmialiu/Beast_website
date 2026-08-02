c927ae5f1fecc4028e5329d11fae7f2642a83d1b
Sarah Mia Liu <sarahmia.liu@gmail.com>
2026-08-02 14:59:32 -0700

    troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 const { defineConfig } = require('vite')
 const tailwindcss = require('@tailwindcss/vite').default
+const path = require('path')
 
 module.exports = defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        academicsFirstYear: path.resolve(__dirname, 'academics_firstyear.html'),
+        academicsCourses: path.resolve(__dirname, 'academics_courses.html'),
+        academicsTeaching: path.resolve(__dirname, 'academics_teaching.html'),
+        quals: path.resolve(__dirname, 'quals.html'),
+        dissertation: path.resolve(__dirname, 'dissertation.html'),
+        faqs: path.resolve(__dirname, 'faqs.html'),
+        directory: path.resolve(__dirname, 'directory.html'),
+        forms: path.resolve(__dirname, 'forms.html'),
+        movingToCa: path.resolve(__dirname, 'movingtoca.html'),
+        // add every other .html file in your project here
+      },
+    },
+  },
 })
